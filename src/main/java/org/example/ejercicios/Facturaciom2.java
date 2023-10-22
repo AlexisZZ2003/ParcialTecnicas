b4d169e91de13c4978a50a1f9c6e5fa38a00eabc
AlexisZZ2003 <141886437+AlexisZZ2003@users.noreply.github.com>
2023-10-22 12:03:40 -0500

    Update Facturaciom2.java

--- src/main/java/org/example/ejercicios/Facturaciom2.java
+++ src/main/java/org/example/ejercicios/Facturaciom2.java
@@ -2,30 +2,29 @@ package org.example.ejercicios;
 
 import java.util.List;
 
-public class Facturaciom2 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
+public class Facturacion {
+	record Factura(String cliente, String fechaFactura, List<Double> precioProducto) {
 	}
 
 	public static void main(String[] args) {
 		Factura factura1 = new Factura("Maria Sannabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
 		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
 
-		double totalValorFact1 = 0;
-		for (Double precio : factura1.precioProducto) {
-			totalValorFact1 += precio;
-		}
-		System.out.println("Factura 1" );
-		System.out.println("Cliente: " + factura1.cliente());
-		System.out.println("Total: " + totalValorFact1);
-
-
-		double totalValorFact2 = 0;
-		for (Double precio : factura2.precioProducto) {
-			totalValorFact2 += precio;
-		}
-		System.out.println("Factura 2" );
-		System.out.println("Cliente: " + factura2.cliente());
-		System.out.println("Total: " + totalValorFact2);
+		double totalValorFactura1 = calcularTotal(factura1);
+                imprimirFactura("Factura 1", factura1, totalValorFactura1);
+		
+                double totalValorFactura2 = calcularTotal(factura2);
+                imprimirFactura("Factura 2", factura2, totalValorFactura2);
 	}
 
+	private static double calcularTotal(Factura factura) {
+            double total = factura.preciosProducto().stream().mapToDouble(Double::doubleValue).sum();
+            return total;		
+	}
+
+	private static void imprimirFactura(String titulo, Factura factura, double total) {
+            System.out.println(titulo);
+            System.out.println("Cliente: " + factura.cliente());
+            System.out.println("Total: " + total);
+        }
 }
